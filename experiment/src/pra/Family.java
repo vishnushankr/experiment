ef9a8a75cd6ed667fcc5e0fdd5968644fadcb8a8
mprathiba <mprathiba@mprathiba-sa.stratapps.com>
2022-11-09 14:10:26 +0530

    m

---
+++ experiment/src/pra/Family.java
@@ -0,0 +1,7 @@
+package pra;
+
+public class Family {
+public static void main(String[] args) {
+	System.out.println("prathibha husband srikanth and their daughter anvika it is a beautiful family");
+}
+}
